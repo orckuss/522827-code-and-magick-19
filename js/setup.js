c8b6721a5fafd6e06efaeeae1e501994da17dd60
Dmitry Vasilev <orckuss@gmail.com>
2020-02-03 23:37:19 +0300

    Add show/hide setup

--- js/setup.js
+++ js/setup.js
@@ -41,6 +41,9 @@ var EYES_COLORS_MOCK = [
   'green',
 ];
 
+var ESC_KEY = 'Escape';
+var ENTER_KEY = 'Enter';
+
 var getRandomValueFromMock = function (mock) {
   var lastIndex = mock.length - 1;
   return mock[Math.round(Math.random() * lastIndex)];
@@ -90,17 +93,53 @@ var renderWizards = function (players) {
   wizardList.appendChild(fragment);
 };
 
-var showSetupWindow = function () {
-  var setupDialog = document.querySelector('.setup');
-
-  setupDialog.classList.remove('hidden');
-
-  setupDialog.querySelector('.setup-similar')
-    .classList.remove('hidden');
-};
-
 var players = generatePlayers(PLAYERS_COUNT);
 
 renderWizards(players);
 
-showSetupWindow();
+var setupDialog = document.querySelector('.setup');
+var openSetupButton = document.querySelector('.setup-open');
+var closeSetupButton = setupDialog.querySelector('.setup-close');
+var setupInputName = setupDialog.querySelector('.setup-user-name');
+
+var onEscPressed = function (evt) {
+  if ((evt.key === ESC_KEY) && (document.activeElement !== setupInputName)) {
+    hideSetupWindow();
+  }
+};
+
+var showSetupWindow = function () {
+  setupDialog.classList.remove('hidden');
+  setupDialog.querySelector('.setup-similar')
+    .classList.remove('hidden');
+
+  document.addEventListener('keydown', onEscPressed);
+};
+
+var hideSetupWindow = function () {
+  setupDialog.classList.add('hidden');
+  setupDialog.querySelector('.setup-similar')
+    .classList.add('hidden');
+
+  document.removeEventListener('keydown', onEscPressed);
+};
+
+openSetupButton.addEventListener('click', function () {
+  showSetupWindow();
+});
+
+openSetupButton.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    showSetupWindow();
+  }
+});
+
+closeSetupButton.addEventListener('click', function () {
+  hideSetupWindow();
+});
+
+closeSetupButton.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    hideSetupWindow();
+  }
+});
